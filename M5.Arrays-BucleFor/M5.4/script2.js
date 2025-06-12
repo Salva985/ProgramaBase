3bb67e563e638ed51f2011abf69ad3944032bb65
salvatore marchese <smarchese985@yahoo.co.uk>
2025-06-12 15:28:02 +0200

    refactoring

--- M5.Arrays-BucleFor/M5.4/script2.js
+++ M5.Arrays-BucleFor/M5.4/script2.js
@@ -1,22 +1,28 @@
 function guessNumber() {
-    let randomNumber = Math.ceil(Math.random() * 10);
-    let guess;
-    let attempts = 0;
-    const maxAttempts = 3;
+  let randomNumber = Math.ceil(Math.random() * 10);
+  let guess;
+  let attempts = 0;
+  const maxAttempts = 3;
 
-    do {
-        guess = parseInt(prompt("Guess the number:"));
-        if(guess < randomNumber){
-            alert("the number is bigger!")
-        } else if (guess < randomNumber){
-            alert("The number is smaller!")
-        }
-        attempts = attempts + 1;  
-    } while (guess !== randomNumber && attempts < maxAttempts);
+  do {
+    guess = parseInt(prompt("Guess the number:"));
+    if (guess < randomNumber) {
+      alert("the number is bigger!");
+    } else if (guess < randomNumber) {
+      alert("The number is smaller!");
+    }
+    attempts = attempts + 1;
+  } while (guess !== randomNumber && attempts < maxAttempts);
 
-    if(randomNumber === guess){
-        alert("Congratulations, the number is " + randomNumber + " and you needed " + attempts + " attempts to achive it!")
-    }else {
-        alert("You havce used all the attempts 😵‍💫 The number was " + randomNumber)
-    }    
+  if (randomNumber === guess) {
+    alert(
+      "Congratulations, the number is " +
+        randomNumber +
+        " and you needed " +
+        attempts +
+        " attempts to achive it!"
+    );
+  } else {
+    alert("You havce used all the attempts 😵‍💫 The number was " + randomNumber);
+  }
 }
